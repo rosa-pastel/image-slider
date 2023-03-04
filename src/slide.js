ea0c7c8a828e0286853750063e7a6991f384d568
rosa-pastel <zeynepranamutlu@outlook.com>
2023-03-04 20:19:46 +0300

    Add navigation circles and auto-slide

--- src/slide.js
+++ src/slide.js
@@ -1,28 +1,60 @@
+function convert(num) {
+  if (typeof num === "number")
+    switch (num) {
+      case 0:
+        return "zero";
+      case 1:
+        return "one";
+      case 2:
+        return "two";
+      case 3:
+        return "three";
+      default:
+        return "four";
+    }
+  else {
+    switch (num) {
+      case "zero":
+        return 0;
+      case "one":
+        return 1;
+      case "two":
+        return 2;
+      case "three":
+        return 3;
+      default:
+        return 4;
+    }
+  }
+}
 function slide(direction) {
   const images = Array.from(document.getElementsByClassName("image"));
-  images.map((image) => {
-    let id;
-    switch (image.id) {
-      case "four":
-        id = direction === "right" ? "zero" : "three";
-        break;
-      case "zero":
-        id = direction === "right" ? "one" : "four";
-        break;
-      case "one":
-        id = direction === "right" ? "two" : "zero";
-        break;
-      case "two":
-        id = direction === "right" ? "three" : "one";
-        break;
-      default:
-        id = direction === "right" ? "four" : "two";
-        break;
+  const circles = Array.from(document.getElementsByClassName("circle"));
+  images.map((image, imgIndex) => {
+    const oldPos = convert(image.id);
+    let newPos;
+    if (direction === "right") {
+      newPos = oldPos === 4 ? 0 : oldPos + 1;
+    } else {
+      newPos = oldPos === 0 ? 4 : oldPos - 1;
     }
-    image.setAttribute("id", id);
+    const imgCircle = circles[imgIndex];
+    if (newPos === 2) {
+      imgCircle.classList.add("circle-full");
+    } else {
+      imgCircle.classList.remove("circle-full");
+    }
+    image.setAttribute("id", convert(newPos));
   });
 }
-
+function goToSlide(circleIndex) {
+  const image = document
+    .getElementById("image-container")
+    .children.item(circleIndex);
+  while (image.id !== "two") {
+    slide("left");
+  }
+}
 export default function initSlider() {
   const rightArrow = document.getElementById("arrow-right");
   rightArrow.addEventListener("click", () => {
@@ -32,4 +64,9 @@ export default function initSlider() {
   leftArrow.addEventListener("click", () => {
     slide("left");
   });
+  const circles = Array.from(document.getElementsByClassName("circle"));
+  circles.map((circle, index) => {
+    circle.addEventListener("click", goToSlide.bind(circle, index));
+  });
+  setInterval(slide, 5000);
 }
